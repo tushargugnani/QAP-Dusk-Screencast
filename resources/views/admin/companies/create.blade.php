00aedd04d591b9bf06530f42d8fd48c962ec7287
Tushar Gugnani <tushar.gugnani@gmail.com>
2019-12-30 18:22:06 +0530

    View file changes

--- resources/views/admin/companies/create.blade.php
+++ resources/views/admin/companies/create.blade.php
@@ -47,7 +47,7 @@
             </div>
             <div class="form-group {{ $errors->has('city_id') ? 'has-error' : '' }}">
                 <label for="city">{{ trans('cruds.company.fields.city') }}</label>
-                <select name="city_id" id="city" class="form-control select2">
+                <select name="city_id" id="city" class="form-control">
                     @foreach($cities as $id => $city)
                         <option value="{{ $id }}" {{ (isset($company) && $company->city ? $company->city->id : old('city_id')) == $id ? 'selected' : '' }}>{{ $city }}</option>
                     @endforeach
@@ -59,10 +59,8 @@
                 @endif
             </div>
             <div class="form-group {{ $errors->has('categories') ? 'has-error' : '' }}">
-                <label for="categories">{{ trans('cruds.company.fields.categories') }}
-                    <span class="btn btn-info btn-xs select-all">{{ trans('global.select_all') }}</span>
-                    <span class="btn btn-info btn-xs deselect-all">{{ trans('global.deselect_all') }}</span></label>
-                <select name="categories[]" id="categories" class="form-control select2" multiple="multiple">
+                <label for="categories">{{ trans('cruds.company.fields.categories') }}</label>
+                <select name="categories[]" id="categories" class="form-control">
                     @foreach($categories as $id => $categories)
                         <option value="{{ $id }}" {{ (in_array($id, old('categories', [])) || isset($company) && $company->categories->contains($id)) ? 'selected' : '' }}>{{ $categories }}</option>
                     @endforeach
@@ -78,9 +76,7 @@
             </div>
             <div class="form-group {{ $errors->has('logo') ? 'has-error' : '' }}">
                 <label for="logo">{{ trans('cruds.company.fields.logo') }}</label>
-                <div class="needsclick dropzone" id="logo-dropzone">
-
-                </div>
+                <input type="file" name="logo" class="form-control"/>
                 @if($errors->has('logo'))
                     <em class="invalid-feedback">
                         {{ $errors->first('logo') }}
@@ -101,58 +97,5 @@
 @endsection
 
 @section('scripts')
-<script>
-    Dropzone.options.logoDropzone = {
-    url: '{{ route('admin.companies.storeMedia') }}',
-    maxFilesize: 2, // MB
-    acceptedFiles: '.jpeg,.jpg,.png,.gif',
-    maxFiles: 1,
-    addRemoveLinks: true,
-    headers: {
-      'X-CSRF-TOKEN': "{{ csrf_token() }}"
-    },
-    params: {
-      size: 2,
-      width: 4096,
-      height: 4096
-    },
-    success: function (file, response) {
-      $('form').find('input[name="logo"]').remove()
-      $('form').append('<input type="hidden" name="logo" value="' + response.name + '">')
-    },
-    removedfile: function (file) {
-      file.previewElement.remove()
-      if (file.status !== 'error') {
-        $('form').find('input[name="logo"]').remove()
-        this.options.maxFiles = this.options.maxFiles + 1
-      }
-    },
-    init: function () {
-@if(isset($company) && $company->logo)
-      var file = {!! json_encode($company->logo) !!}
-          this.options.addedfile.call(this, file)
-      this.options.thumbnail.call(this, file, file.url)
-      file.previewElement.classList.add('dz-complete')
-      $('form').append('<input type="hidden" name="logo" value="' + file.file_name + '">')
-      this.options.maxFiles = this.options.maxFiles - 1
-@endif
-    },
-    error: function (file, response) {
-        if ($.type(response) === 'string') {
-            var message = response //dropzone sends it's own error messages in string
-        } else {
-            var message = response.errors.file
-        }
-        file.previewElement.classList.add('dz-error')
-        _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
-        _results = []
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            node = _ref[_i]
-            _results.push(node.textContent = message)
-        }
 
-        return _results
-    }
-}
-</script>
 @stop
